df07adcc3767ee84a99074c88fc53edc228eb6c5
Kriti <agrawalkriti0303@gmail.com>
2023-01-29 00:43:02 +0530

    passport authentication

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 const { maxBy } = require('lodash');
-const saltRounds = 10;
+const session=require('express-session');
+const passport=require('passport');
+const passportLocalMongoose=require('passport-local-mongoose');
 
 
 const app = express();
@@ -15,6 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret:"Thisisourlittlesecret.",
+    resave:false,
+    saveUninitialized:false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 mongoose.set('strictQuery', true);
@@ -26,10 +35,14 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
+userSchema.plugin(passportLocalMongoose);
 
 
 
 const User = new mongoose.model("User", userSchema);
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
     res.render("home");
@@ -43,38 +56,77 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.get("/secrets",function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }
+    else{
+        res.redirect("/login");
+    }
+});
 
+app.get("/logout",function(req,res){
+    req.logout(function(){});
+    res.redirect("/");
+});
 
 app.post("/register", function (req, res) {
 
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+    // bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
 
-        const newUser = new User({
-            email: req.body.username,
-            password: hash
-        });
-        newUser.save(function (err) {
-            if (err) res.send("Error registereing");
-            else res.render("secrets");
-        });
+    //     const newUser = new User({
+    //         email: req.body.username,
+    //         password: hash
+    //     });
+    //     newUser.save(function (err) {
+    //         if (err) res.send("Error registereing");
+    //         else res.render("secrets");
+    //     });
+    // });
+    User.register({username:req.body.username},req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }
+        else {
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
+        }
     });
+
 });
 
 app.post("/login", function (req, res) {
-    User.findOne({ email: req.body.username }, function (err, result) {
-        if (!err) {
-            if (result) {
-                bcrypt.compare(req.body.password, result.password, function (err, match) {
+    // User.findOne({ email: req.body.username }, function (err, result) {
+    //     if (!err) {
+    //         if (result) {
+    //             bcrypt.compare(req.body.password, result.password, function (err, match) {
 
-                    if (match==true) { res.render("secrets"); }
-                    else res.send("Password Mismatched!!");
+    //                 if (match==true) { res.render("secrets"); }
+    //                 else res.send("Password Mismatched!! ");
 
-                });
-            }
-            else res.send("User not found");
+    //             });
+    //         }
+    //         else res.send("User not found");
+    //     }
+
+    //     else res.send(err);
+    // });
+    const user=new User({
+        username:req.body.username,
+        password:req.body.password
+    });
+
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
+
+        }else {
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets");
+            });
         }
-
-        else res.send(err);
     });
 });
 
